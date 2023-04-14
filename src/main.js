9c6d03e19a8f6fbd9169a6dd8533827c4d9490c4
Antonio Souza <antonios16020@gmail.com>
2023-04-14 13:44:32 -0300

    add i18n

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import './scss/main.scss';
 import LocomotiveScroll from 'locomotive-scroll';
+import initTranslations from './scripts/i18n'
 
 const scroll = new LocomotiveScroll();
 
 const { BASE_URL } = import.meta.env
 
+initTranslations();
---
+++ src/scripts/i18n.js
@@ -0,0 +1,53 @@
+import i18next from "i18next";
+
+const loadLanguages = () => {
+  return fetch('src/utils/languages.json')
+    .then(response => response.json());
+};
+
+const getTranslationKey = (el, defaultKey) => {
+  const key = el.dataset.i18n;
+  if (key) {
+    return key;
+  }
+  return defaultKey;
+};
+
+const translateElement = (el, languages) => {
+  const defaultKey = el.innerText;
+  const locale = el.dataset.locale || i18next.language;
+  const key = getTranslationKey(el, defaultKey);
+  const translation = languages[locale][key];
+  el.innerText = translation || defaultKey;
+};
+
+const initTranslations = () => {
+  loadLanguages().then(languages => {
+    i18next.init({
+      lng: 'en',
+      resources: languages,
+      fallbackLng: 'en'
+    }, () => {
+      const elements = document.querySelectorAll("[data-i18n]");
+      elements.forEach(el => {
+        translateElement(el, languages);
+      });
+
+      const langButtons = document.querySelectorAll(".lang-button");
+      langButtons.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+          const locale = e.target.dataset.locale;
+          i18next.changeLanguage(locale);
+          elements.forEach(el => {
+            translateElement(el, languages);
+          });
+        });
+      });
+    });
+  }).catch(err => {
+    console.log(err)
+  }) 
+  ;
+};
+
+export default initTranslations;
