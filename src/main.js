1ae0375b05abb0455141a91d925f90211b62cce4
Antonio Souza <antonios16020@gmail.com>
2023-04-22 14:56:33 -0300

    add stars background

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import './scss/main.scss';
 import LocomotiveScroll from 'locomotive-scroll';
-import initTranslations from './scripts/i18n'
+import initTranslations from './scripts/i18n';
+import init from './scripts/star';
 
 const scroll = new LocomotiveScroll();
 
 const { BASE_URL } = import.meta.env
 
 initTranslations();
+init();
