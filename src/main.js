c1c7e7e45b154a19bf50d83d364b72ab9dac4fb4
Antonio Souza <antonios16020@gmail.com>
2023-04-23 19:27:48 -0300

    add resume cards

--- src/main.js
+++ src/main.js
@@ -17,16 +17,16 @@ document.querySelector(".header__actions").onmousemove = e => {
   };
 }
 
-// document.querySelector(".resume").onmousemove = e => {
-//   for(const button of document.getElementsByClassName("card")) {
-//     const rect = button.getBoundingClientRect(),
-//           x = e.clientX - rect.left,
-//           y = e.clientY - rect.top;
+document.querySelector(".resume").onmousemove = e => {
+  for(const button of document.getElementsByClassName("card")) {
+    const rect = button.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
 
-//           button.style.setProperty("--mouse-x", `${x}px`);
-//           button.style.setProperty("--mouse-y", `${y}px`);
-//   };
-// }
+          button.style.setProperty("--mouse-x", `${x}px`);
+          button.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
 
 (() => {
   const container = document.querySelector('.about-me__container');
