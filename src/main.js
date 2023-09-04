eb4d39b3b8b46123ce4468809d56b13c8c407e23
Antonio Souza <antonios16020@gmail.com>
2023-09-04 14:31:10 -0300

    remove locomotive scroll

--- src/main.js
+++ src/main.js
@@ -9,117 +9,52 @@ const { BASE_URL } = import.meta.env;
 addGlow(".header__translate-section", "lang-button");
 addGlow(".resume", "card");
 
-(() => {
-  const container = document.querySelector(".about-me__container");
-  const inner = document.querySelector(".about-me__name");
-  const mouse = {
-    _x: 0,
-    _y: 0,
-    x: 0,
-    y: 0,
-    updatePosition(event) {
-      const { clientX, clientY } = event;
-      this.x = clientX - this._x;
-      this.y = (clientY - this._y) * -1;
-    },
-    setOrigin(e) {
-      this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
-      this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
-    },
-    show() {
-      return `(${this.x}, ${this.y})`;
-    },
+
+(()=> {
+
+  var doc = document.documentElement;
+  var w = window;
+
+  var prevScroll = w.scrollY || doc.scrollTop;
+  var curScroll;
+  var direction = 0;
+  var prevDirection = 0;
+
+  var header = document.querySelector('.header__actions');
+
+  var checkScroll = function() {
+
+    curScroll = w.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) { 
+      direction = 2;
+    }
+    else if (curScroll < prevScroll) { 
+      direction = 1;
+    }
+
+    if (direction !== prevDirection) {
+      toggleHeader(direction, curScroll);
+    }
+    
+    prevScroll = curScroll;
   };
 
-  mouse.setOrigin(container);
-
-  let counter = 0;
-  const updateRate = 10;
-  const isTimeToUpdate = () => counter++ % updateRate === 0;
-
-  const onMouseEnterHandler = (event) => update(event);
-
-  const onMouseLeaveHandler = () => {
-    inner.style = "";
-  };
-
-  const onMouseMoveHandler = (event) => {
-    if (isTimeToUpdate()) {
-      update(event);
+  var toggleHeader = function(direction, curScroll) {
+    if (direction === 2 && curScroll > 52) { 
+      
+      header.classList.add('hide');
+      prevDirection = direction;
+    }
+    else if (direction === 1) {
+      header.classList.remove('hide');
+      prevDirection = direction;
     }
   };
+  
+  window.addEventListener('scroll', checkScroll);
 
-  const update = (event) => {
-    mouse.updatePosition(event);
-    const { offsetWidth, offsetHeight } = inner;
-    const x = (mouse.y / offsetHeight / 2).toFixed(2);
-    const y = (mouse.x / offsetWidth / 2).toFixed(2);
-    updateTransformStyle(`rotateX(${x}deg) rotateY(${y}deg)`);
-  };
-
-  const updateTransformStyle = (style) => {
-    inner.style.transform = style;
-    inner.style.webkitTransform = style;
-    inner.style.mozTransform = style;
-    inner.style.msTransform = style;
-    inner.style.oTransform = style;
-  };
-
-  container.addEventListener("mouseenter", onMouseEnterHandler);
-  container.addEventListener("mouseleave", onMouseLeaveHandler);
-  container.addEventListener("mousemove", onMouseMoveHandler);
 })();
 
-const sidebar = document.querySelector(".sidebar");
-
-const scrollOptions = {
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
-  mobile: {
-    smooth: true,
-  },
-  tablet: {
-    smooth: true,
-    breakpoint: 0,
-  },
-};
-
-let scroll;
-
-const initializeScroll = () => {
-  scroll = new LocomotiveScroll(scrollOptions);
-};
-
-const destroyScroll = () => {
-  if (scroll) {
-    scroll.destroy();
-    scroll = null;
-  }
-};
-
-window.addEventListener("load", () => {
-  initializeScroll()
-});
-
-
-const setDataScroll = () => {
-  if (window.innerWidth < 1000) {
-    sidebar.removeAttribute("data-scroll");
-    sidebar.removeAttribute("data-scroll-sticky");
-    sidebar.removeAttribute("data-scroll-target");
-    sidebar.removeAttribute("data-scroll-offset");
-    sidebar.removeAttribute("style");
-    destroyScroll();
-    return;
-  }
-  sidebar.setAttribute("data-scroll", "");
-  sidebar.setAttribute("data-scroll-sticky", "");
-  sidebar.setAttribute("data-scroll-target", ".resume");
-  sidebar.setAttribute("data-scroll-offset", "-20, 48");
-};
-
-setDataScroll();
-window.addEventListener("resize", setDataScroll);
 
 initTranslations();
 init();
