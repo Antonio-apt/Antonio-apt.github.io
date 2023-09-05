039c5a37ae4ec8a641da8a7cddd39cc957cec563
Antonio Souza <antonios16020@gmail.com>
2023-09-05 15:21:31 -0300

    add vanilla tilt

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
 import "./styles/main.scss";
-import LocomotiveScroll from "locomotive-scroll";
 import initTranslations from "./scripts/i18n";
 import init from "./scripts/star";
 import addGlow from "./scripts/glow";
+import AnimateScroll from "./scripts/animate-scroll";
+import MouseHoverTilt from "./scripts/tilt";
 
 const { BASE_URL } = import.meta.env;
 
+const animateScroll = new AnimateScroll("[data-anime='scroll']");
+animateScroll.init();
+// const mouseHoverAnimation = new MouseHoverAnimation("[data-anime='perspective']", "[data-anime='inner']");
+// mouseHoverAnimation.init();
+
 addGlow(".header__translate-section", "lang-button");
-addGlow(".resume", "card");
+addGlow("main", "card");
 
 
-(()=> {
-
+(() => {
   var doc = document.documentElement;
   var w = window;
 
@@ -20,95 +25,54 @@ addGlow(".resume", "card");
   var direction = 0;
   var prevDirection = 0;
 
-  var header = document.querySelector('.header__actions');
+  var header = document.querySelector(".header__actions");
 
-  var checkScroll = function() {
-
+  var checkScroll = function () {
     curScroll = w.scrollY || doc.scrollTop;
-    if (curScroll > prevScroll) { 
+    if (curScroll > prevScroll) {
       direction = 2;
-    }
-    else if (curScroll < prevScroll) { 
+    } else if (curScroll < prevScroll) {
       direction = 1;
     }
 
     if (direction !== prevDirection) {
       toggleHeader(direction, curScroll);
     }
-    
+
     prevScroll = curScroll;
   };
 
-  var toggleHeader = function(direction, curScroll) {
-    if (direction === 2 && curScroll > 52) { 
-      
-      header.classList.add('hide');
+  var toggleHeader = function (direction, curScroll) {
+    if (direction === 2 && curScroll > 52) {
+      header.classList.add("hide");
       prevDirection = direction;
-    }
-    else if (direction === 1) {
-      header.classList.remove('hide');
+    } else if (direction === 1) {
+      header.classList.remove("hide");
       prevDirection = direction;
     }
   };
-  
-  window.addEventListener('scroll', checkScroll);
 
+  window.addEventListener("scroll", checkScroll);
 })();
 
-const zoom = document.querySelector('.carrousels');
-const zoomText = document.querySelectorAll('.carrousel');
+const zoom = document.querySelector(".carrousels");
+const zoomText = document.querySelectorAll(".carrousel");
 const minZoom = 1;
 const maxZoom = 1.5;
 
-addEventListener('scroll', e => {
-	const vh = window.innerHeight / 100;
+addEventListener("scroll", (e) => {
+  const vh = window.innerHeight / 100;
   const scrollTop = document.documentElement.scrollTop;
   const start = 100 * vh;
   const stop = 200 * vh;
   if (scrollTop > start && scrollTop < stop) {
-	  const scale = Math.max(maxZoom - (scrollTop - start) / 500, minZoom);
-  	zoom.style.transform = `scale(1, ${scale})`
-    zoomText.forEach(text => {
-  	  text.style.transform = `scale(${scale}, 1)`
-    })
+    const scale = Math.max(maxZoom - (scrollTop - start) / 500, minZoom);
+    zoom.style.transform = `scale(1, ${scale})`;
+    zoomText.forEach((text) => {
+      text.style.transform = `scale(${scale}, 1)`;
+    });
   }
-})
-
-const content = document.querySelector(".stack-description");
-const maxSkew = 2;
-const maxRotate = 2;
-
-let currentPosition = window.pageYOffset;
-
-function skewEffect() {
-  const newPosition = window.pageYOffset;
-  const dif = newPosition - currentPosition;
-
-  let skew = dif * 0.8;
-  let rotate = dif * 0.2;
-  if (skew > maxSkew || skew < -maxSkew) {
-    if (skew > 0) {
-      skew = maxSkew;
-    } else if (skew < 0) {
-      skew = -maxSkew;
-    }
-  }
-  if (rotate > maxRotate || rotate < -maxRotate) {
-    if (rotate > 0) {
-      rotate = maxRotate;
-    } else if (skew < 0) {
-      rotate = -maxRotate;
-    }
-  }
-
-  content.style.transform = `skewY(${skew}deg) rotateY(${rotate}deg)`;
-  currentPosition = newPosition;
-  requestAnimationFrame(skewEffect);
-}
-
-skewEffect();
-
-
+});
 
 initTranslations();
 init();
