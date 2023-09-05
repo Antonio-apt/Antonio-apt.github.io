8f3ea09871800e54b78625b775a9138b134b72f4
Antonio Souza <antonios16020@gmail.com>
2023-09-04 22:55:17 -0300

    improve stack section

--- src/main.js
+++ src/main.js
@@ -55,6 +55,60 @@ addGlow(".resume", "card");
 
 })();
 
+const zoom = document.querySelector('.carrousels');
+const zoomText = document.querySelectorAll('.carrousel');
+const minZoom = 1;
+const maxZoom = 1.5;
+
+addEventListener('scroll', e => {
+	const vh = window.innerHeight / 100;
+  const scrollTop = document.documentElement.scrollTop;
+  const start = 100 * vh;
+  const stop = 200 * vh;
+  if (scrollTop > start && scrollTop < stop) {
+	  const scale = Math.max(maxZoom - (scrollTop - start) / 500, minZoom);
+  	zoom.style.transform = `scale(1, ${scale})`
+    zoomText.forEach(text => {
+  	  text.style.transform = `scale(${scale}, 1)`
+    })
+  }
+})
+
+const content = document.querySelector(".stack-description");
+const maxSkew = 2;
+const maxRotate = 2;
+
+let currentPosition = window.pageYOffset;
+
+function skewEffect() {
+  const newPosition = window.pageYOffset;
+  const dif = newPosition - currentPosition;
+
+  let skew = dif * 0.8;
+  let rotate = dif * 0.2;
+  if (skew > maxSkew || skew < -maxSkew) {
+    if (skew > 0) {
+      skew = maxSkew;
+    } else if (skew < 0) {
+      skew = -maxSkew;
+    }
+  }
+  if (rotate > maxRotate || rotate < -maxRotate) {
+    if (rotate > 0) {
+      rotate = maxRotate;
+    } else if (skew < 0) {
+      rotate = -maxRotate;
+    }
+  }
+
+  content.style.transform = `skewY(${skew}deg) rotateY(${rotate}deg)`;
+  currentPosition = newPosition;
+  requestAnimationFrame(skewEffect);
+}
+
+skewEffect();
+
+
 
 initTranslations();
 init();
