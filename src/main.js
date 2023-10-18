c675fe15d3253e70e57eb863d29df976e3631019
Antonio Souza <antonios16020@gmail.com>
2023-10-18 12:06:37 -0300

    add base mobile header

--- src/main.js
+++ src/main.js
@@ -13,6 +13,25 @@ animateScroll.init();
 addGlow(".header__translate-section", "lang-button");
 addGlow("main", "card");
 
+let burger = document.getElementById('burger');
+let nav = document.getElementById('main-nav');
+
+burger.addEventListener('click', function(e){
+	this.classList.toggle('is-open');
+	nav.classList.toggle('is-open');
+});
+
+/* Onload demo - dirty timeout */
+let clickEvent = new Event('click');
+
+window.addEventListener('load', function(e) {
+	burger.dispatchEvent(clickEvent);
+	
+	setTimeout(function(){
+		burger.dispatchEvent(clickEvent);
+	}, 5500);
+});
+
 
 (() => {
   var doc = document.documentElement;
